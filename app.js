f5031c5b757452c5fd1624903b1f40ec572f710a
Baturova Olga <batolser@gmail.com>
2020-09-07 11:01:21 +0300

    Добавление настроек cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
@@ -10,9 +11,23 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
 const { PORT } = require('./config');
 
+var whitelist = ['http://localhost:8080', 'https://api.news-bord-api.cf', 'http://api.news-bord-api.cf'];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+};
+
+
 const router = require('./routes/index');
 
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
