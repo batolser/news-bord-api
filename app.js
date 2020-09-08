a21e2fc361490164931f2b5e3520aa4f6199c45a
Baturova Olga <batolser@gmail.com>
2020-09-08 16:38:47 +0300

    sameSite: 'none'

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-const { PORT } = require('./config');
+const { PORT, DB } = require('./config');
 
 var whitelist = ['http://localhost:8080', 'https://api.news-bord-api.cf', 'http://api.news-bord-api.cf'];
 var corsOptions = {
@@ -31,7 +33,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/news-bord_db', {
+mongoose.connect(DB, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: parseInt(process.env.PORT, 10) || 3000,
   JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
+  DB: 'mongodb://localhost:27017/news-bord_db',
 };
